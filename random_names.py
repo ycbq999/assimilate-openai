b76ca27655cc5c8ed2d5219a01f763ae1c5421a5
STC <ycbq999@hotmail.com>
2023-08-12 06:48:10 +0000

    new addition

--- random_names.py
+++ random_names.py
@@ -1,8 +1,62 @@
 import random
 
-first_names = ['John', 'Jane', 'Corey', 'Travis', 'Dave', 'Kurt', 'Neil', 'Sam', 'Steve', 'Tom', 'James', 'Robert', 'Michael', 'Charles', 'Joe', 'Mary', 'Maggie', 'Nicole', 'Patricia', 'Linda', 'Barbara', 'Elizabeth', 'Laura', 'Jennifer', 'Maria']
+first_names = [
+    "John",
+    "Jane",
+    "Corey",
+    "Travis",
+    "Dave",
+    "Kurt",
+    "Neil",
+    "Sam",
+    "Steve",
+    "Tom",
+    "James",
+    "Robert",
+    "Michael",
+    "Charles",
+    "Joe",
+    "Mary",
+    "Maggie",
+    "Nicole",
+    "Patricia",
+    "Linda",
+    "Barbara",
+    "Elizabeth",
+    "Laura",
+    "Jennifer",
+    "Maria",
+]
 
-last_names = ['Smith', 'Doe', 'Jenkins', 'Robinson', 'Davis', 'Stuart', 'Jefferson', 'Jacobs', 'Wright', 'Patterson', 'Wilks', 'Arnold', 'Johnson', 'Williams', 'Jones', 'Brown', 'Davis', 'Miller', 'Wilson', 'Moore', 'Taylor', 'Anderson', 'Thomas', 'Jackson', 'White', 'Harris', 'Martin']
+last_names = [
+    "Smith",
+    "Doe",
+    "Jenkins",
+    "Robinson",
+    "Davis",
+    "Stuart",
+    "Jefferson",
+    "Jacobs",
+    "Wright",
+    "Patterson",
+    "Wilks",
+    "Arnold",
+    "Johnson",
+    "Williams",
+    "Jones",
+    "Brown",
+    "Davis",
+    "Miller",
+    "Wilson",
+    "Moore",
+    "Taylor",
+    "Anderson",
+    "Thomas",
+    "Jackson",
+    "White",
+    "Harris",
+    "Martin",
+]
 
 full_names = []
 
